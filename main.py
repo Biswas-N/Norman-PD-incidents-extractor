9a590a76872c7797ce7114df6faf019745fca0bf
Biswas Nandamuri <biswas.bin2@gmail.com>
2022-03-06 00:14:56 -0600

    (feat) Add main.py to accept cmd arguments and pull data

---
+++ main.py
@@ -0,0 +1,28 @@
+import argparse
+
+from project0 import downloader, parser, storer
+
+
+def main(url):
+    # Download data
+    incident_data = downloader.fetch_incidents(url)
+
+    # Extract data
+    incidents = parser.extract_incidents(incident_data)
+
+    # Create new database
+    db = storer.create_db()
+    inserted_count = db.add_incidents(incidents)
+
+    if(inserted_count != -1):
+        print(f'\n{db.get_stats()}')
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--incidents", type=str, required=True,
+                            help="Incident summary url.")
+
+    args = arg_parser.parse_args()
+    if args.incidents:
+        main(args.incidents)
